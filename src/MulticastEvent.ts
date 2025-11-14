76d7235e48eb15d71f2cd482449a1456973f92b3
Alex Rothman <40244184+aprothman@users.noreply.github.com>
2025-11-14 10:40:22 -0500

    Use a Set instead of an Array to manage callbacks

--- src/MulticastEvent.ts
+++ src/MulticastEvent.ts
@@ -1,16 +1,16 @@
 import type { EventCallback } from '../type/types.ts';
 
 export default class MulticastEvent<TEventArg> {
-  callbacks: EventCallback<TEventArg>[] = [];
+  callbacks = new Set<EventCallback<TEventArg>>();
 
-  removeList: EventCallback<TEventArg>[] = [];
+  removeList = new Set<EventCallback<TEventArg>>();
 
   /**
    * Add to the list of callbacks for this event
    * @param callback function to be called when the event is raised
    */
   registerCallback(callback: EventCallback<TEventArg>) {
-    this.callbacks.push(callback);
+    this.callbacks.add(callback);
   }
 
   /**
@@ -19,32 +19,19 @@ export default class MulticastEvent<TEventArg> {
    * @param callback function to be called when the event is raised
    */
   registerCallbackWithRemoval(callback: EventCallback<TEventArg>) {
-    this.removeList.push(callback);
-    this.callbacks.push(callback);
+    this.removeList.add(callback);
+    this.callbacks.add(callback);
   }
 
   /**
    * If the callback parameter is registered for this event, unregister it.
    * @param callback function that was previously registered
-   * @param cleanRemoveList if true, additionally remove the function from the removeList
    * @returns true if the callback function was found and removed
    */
-  unregisterCallback(callback: EventCallback<TEventArg>, cleanRemoveList = true) {
-    if (cleanRemoveList) {
-      // first remove it from the removeList
-      const removeIndex = this.removeList.indexOf(callback);
-      if (removeIndex > -1) {
-        this.removeList.splice(removeIndex, 1);
-      }
-    }
+  unregisterCallback(callback: EventCallback<TEventArg>) {
+    this.removeList.delete(callback);
 
-    const index = this.callbacks.indexOf(callback);
-    if (index > -1) {
-      this.callbacks.splice(index, 1);
-      return true;
-    }
-
-    return false;
+    return this.callbacks.delete(callback);
   }
 
   /**
@@ -52,14 +39,9 @@ export default class MulticastEvent<TEventArg> {
    * @param arg event argument passed to each callback
    */
   raiseEvent(arg: TEventArg) {
-    const callList = [...this.callbacks];
-
-    callList.forEach((callback) => {
-      // first check if the callback is on the list to be removed
-      const index = this.removeList.indexOf(callback);
-      if (index > -1) {
-        this.removeList.splice(index, 1);
-        this.unregisterCallback(callback, false);
+    this.callbacks.forEach((callback) => {
+      if (this.removeList.has(callback)) {
+        this.unregisterCallback(callback);
       }
 
       callback(arg);
